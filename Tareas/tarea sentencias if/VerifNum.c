0131773cf400d1f6c5000b9ab12c32daa1fcbc77
Marcelo767n <luismarcelo767n@gmail.com>
2025-04-08 12:05:45 -0400

    ejercicio 1 y 2 terminado

---
+++ Tareas/tarea sentencias if/VerifNum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// Verifica si un número es positivo, negativo o cero
+int main(){
+    int num=0;
+    printf("ingrese un numero\n: ");
+    scanf("%d", &num);
+    if (num>0){
+        printf("el numero %d es positivo\n", num);
+    }else if(num<0){
+        printf("el numero %d es negativo\n", num);
+    }else{
+        printf("el numero %d es cero\n", num);
+    }
+}
